99e9759698df8fe796e3acde9d364b0952a195ca
Simao Ferreira <24299428+simao-ferreira@users.noreply.github.com>
2023-02-16 00:34:04 +0100

    M1: Update brewfile

--- sys/.config/zsh/00-brew.zsh
+++ sys/.config/zsh/00-brew.zsh
@@ -3,6 +3,6 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
 export PATH=/opt/homebrew/bin:$PATH
 
-export JAVA_HOME="$(/usr/libexec/java_home -v 17)"
+export JAVA_HOME="$(/usr/libexec/java_home -v 11)"
 export PATH="/opt/homebrew/opt/python@3.10/bin:$PATH"
 # for rollback `brew link --overwrite python@<version>` and export again.
